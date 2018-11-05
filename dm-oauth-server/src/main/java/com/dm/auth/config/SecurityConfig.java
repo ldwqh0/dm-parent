69bc1600e4710a7838010330e7ea3feb5db0f697
LiDong <ldwqh0@outlook.com>
2018-11-05 17:40:03 +0800

    增加安全配

---
+++ dm-oauth-server/src/main/java/com/dm/auth/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.dm.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
+		auth.userDetailsService(userService);
+	}
+
+}
