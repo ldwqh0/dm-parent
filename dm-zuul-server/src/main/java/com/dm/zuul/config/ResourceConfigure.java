744b7ecfd5ffb4465b7ec872fa0385c1149f425d
LiDong <ldwqh0@outlook.com>
2018-12-11 16:23:38 +0800

    增加单点登录服务

--- dm-zuul-server/src/main/java/com/dm/zuul/config/ResourceConfigure.java
+++ dm-zuul-server/src/main/java/com/dm/zuul/config/ResourceConfigure.java
@@ -1,11 +1,12 @@
 package com.dm.zuul.config;
 
-import java.security.Principal;
 import java.util.Collections;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.access.AccessDecisionManager;
@@ -13,32 +14,23 @@ import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.dm.security.access.RequestAuthoritiesAccessDecisionVoter;
 import com.dm.security.access.RequestAuthoritiesFilterInvocationSecurityMetadataSource;
-import com.dm.security.core.userdetails.UserDetailsDto;
 import com.dm.security.oauth2.provider.token.TokenStoreResourceServerTokenServices;
-import com.dm.security.oauth2.provider.token.UserDetailsAuthenticationConverter;
-import com.dm.security.oauth2.resource.UserDetailsDtoPrincipalExtractor;
 
 @EnableResourceServer
 @EnableWebSecurity
 public class ResourceConfigure extends ResourceServerConfigurerAdapter {
-
-//	@Autowired
-//	private RemoteTokenServices remoteTokenServices;
 
 	@Autowired
 	private RedisConnectionFactory connectionFactory;
@@ -49,10 +41,10 @@ public class ResourceConfigure extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		// 指定所有的资源都要被保护
-		http.authorizeRequests().anyRequest().authenticated();
+		super.configure(http);
 		// 增加自定义的资源授权过滤器
 		http.addFilterBefore(interceptor(), FilterSecurityInterceptor.class);
-		http.antMatcher("/**");
+		http.requestMatcher(new BearerTokenRequestMatcher());
 	}
 
 	@Bean
@@ -75,17 +67,13 @@ public class ResourceConfigure extends ResourceServerConfigurerAdapter {
 		super.configure(resources);
 		// 指定这是一个restful service,不会保存会话状态
 		resources.stateless(true);
-		resources.tokenServices(tokenService());
+//		resources.tokenServices(tokenStoreResourceServerTokenServices());
 	}
 
 	/**
-	 * ResourceServer直接连接Redis读取token信息<br />
-	 * 如果要使用该模式，需要注释掉配置文件中关于security.oauth2相关配置
-	 * 
 	 * @return
 	 */
-	@Bean
-	public ResourceServerTokenServices tokenService() {
+	public ResourceServerTokenServices tokenStoreResourceServerTokenServices() {
 		TokenStoreResourceServerTokenServices tokenService = new TokenStoreResourceServerTokenServices();
 		tokenService.setTokenStore(tokenStore());
 		return tokenService;
@@ -96,47 +84,13 @@ public class ResourceConfigure extends ResourceServerConfigurerAdapter {
 		return new RedisTokenStore(connectionFactory);
 	}
 
-//	/**
-//	 * 指定解码Token信息的解码器
-//	 */
-//	@PostConstruct
-//	public void config() {
-//		remoteTokenServices.setAccessTokenConverter(accessTokenConverter());
-//	}
+	static class BearerTokenRequestMatcher implements RequestMatcher {
+		@Override
+		public boolean matches(HttpServletRequest request) {
+			String authHeader = request.getHeader("Authorization");
+			return StringUtils.startsWithIgnoreCase(authHeader, "bearer");
+		}
 
-	/**
-	 * 如果使用/oauth/check_token 端点解析用户信息，需要额外的转换来处理{@link UserDetails}信息
-	 * 
-	 * @return
-	 */
-	@Bean
-	public AccessTokenConverter accessTokenConverter() {
-		DefaultAccessTokenConverter tokenConverter = new DefaultAccessTokenConverter();
-		tokenConverter.setUserTokenConverter(userTokenConverter());
-		return tokenConverter;
-	}
-
-	/**
-	 * 配置一个Converter，使之可以解析token_info中的Pripical<br />
-	 * 此处将 {@link Principal}转换为一个 {@link UserDetailsDto}对象
-	 * 
-	 * @return
-	 */
-//	@Bean
-	public UserAuthenticationConverter userTokenConverter() {
-		UserDetailsAuthenticationConverter authenticationConverter = new UserDetailsAuthenticationConverter();
-		authenticationConverter.setPrincipalExtractor(principalExtractor());
-		return authenticationConverter;
-	}
-
-	/**
-	 * 指定一个用户信息解码器，将从服务器获取过来的用户信息解码为本地{@link UserDetailsDto}
-	 * 
-	 * @return
-	 */
-//	@Bean
-	public PrincipalExtractor principalExtractor() {
-		return new UserDetailsDtoPrincipalExtractor();
 	}
 
 }
---
+++ dm-zuul-server/src/main/java/com/dm/zuul/config/SsoConfigure.java
@@ -0,0 +1,86 @@
+package com.dm.zuul.config;
+
+import java.security.Principal;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+
+import com.dm.security.core.userdetails.UserDetailsDto;
+import com.dm.security.oauth2.provider.token.UserDetailsAuthenticationConverter;
+import com.dm.security.oauth2.resource.UserDetailsDtoPrincipalExtractor;
+
+@Configuration
+@EnableOAuth2Sso
+@EnableWebSecurity
+public class SsoConfigure extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private FilterSecurityInterceptor filterSecurityInterceptor;
+
+	@Autowired
+	private RemoteTokenServices remoteTokenServices;
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.addFilterBefore(filterSecurityInterceptor, FilterSecurityInterceptor.class);
+	}
+
+	/**
+	 * 如果使用/oauth/check_token 端点解析用户信息，需要额外的转换来处理{@link UserDetails}信息
+	 * 
+	 * @return
+	 */
+	@Bean
+	public AccessTokenConverter accessTokenConverter() {
+		DefaultAccessTokenConverter tokenConverter = new DefaultAccessTokenConverter();
+		tokenConverter.setUserTokenConverter(userTokenConverter());
+		return tokenConverter;
+	}
+
+	/**
+	 * 配置一个Converter，使之可以解析token_info中的Pripical<br />
+	 * 此处将 {@link Principal}转换为一个 {@link UserDetailsDto}对象
+	 * 
+	 * @return
+	 */
+	@Bean
+	public UserAuthenticationConverter userTokenConverter() {
+		UserDetailsAuthenticationConverter authenticationConverter = new UserDetailsAuthenticationConverter();
+		authenticationConverter.setPrincipalExtractor(principalExtractor());
+		return authenticationConverter;
+	}
+
+	/**
+	 * 指定一个用户信息解码器，将从服务器获取过来的用户信息解码为本地{@link UserDetailsDto}
+	 * 
+	 * @return
+	 */
+	@Bean
+	public PrincipalExtractor principalExtractor() {
+		return new UserDetailsDtoPrincipalExtractor();
+	}
+
+	/**
+	 * 指定解码Token信息的解码器
+	 */
+	@PostConstruct
+	public void config() {
+		remoteTokenServices.setAccessTokenConverter(accessTokenConverter());
+	}
+}
