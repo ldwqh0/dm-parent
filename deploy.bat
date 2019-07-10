35cc3c4b7b59f44d42bc58b319f0291eb0dc7d85
xz <ldwqh0@outlook.com>
2019-07-10 14:12:26 +0800

    修改自动推送脚本

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,5 @@
 git.exe push --progress "origin" 0.2.1-SNAPSHOT:0.2.1-SNAPSHOT
+git.exe push --progress "origin" master:master
 git.exe push --progress "aliyun" 0.2.1-SNAPSHOT:develop
 git.exe push --progress "aliyun" 0.2.1-SNAPSHOT:master
 mvn clean deploy -Dmaven.test.skip=true
