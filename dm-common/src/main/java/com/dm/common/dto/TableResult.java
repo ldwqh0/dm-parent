9deb7053fc38f1188bee78f6403ba59eb160254e
LiDong <ldwqh0@outlook.com>
2019-10-21 09:48:42 +0800

    修改表格响应的java doc

--- dm-common/src/main/java/com/dm/common/dto/TableResult.java
+++ dm-common/src/main/java/com/dm/common/dto/TableResult.java
@@ -14,94 +14,112 @@ import org.springframework.data.domain.Pageable;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+/**
+ * 用于表格请求的响应<br>
+ * 一个正确的完整的表格响应需要包括：<br/>
+ * success: <b>必须</b>，查询是否成功的标记<br/>
+ * draw: <b>非必须</b>，请求戳标记.这个标记由客户端提供，服务器原样返回该标记，用以客户端验证某次的请求是否和某次响应相匹配<br>
+ * recordsTotal: <b>必须</b>，查询后未分页的数据总量<br/>
+ * data: <b>必须</b>，在当前分页模式下，返回的数据<br/>
+ * maxId: <b>非必须</b> 在上拉刷新的表格模式中，需要前后端确定maxId,用以确认每次刷新和翻页的数据范围
+ * 如果表格响应错误，则应该在message中包含错误的详情
+ * 
+ * @author LiDong
+ * 
+ * 
+ *
+ * @param <T> 列表的数据类型
+ */
 @JsonInclude(Include.NON_NULL)
 public interface TableResult<T> extends Serializable {
 
-	public Long getDraw();
+    public Long getDraw();
 
-	public Long getPage();
+    public Long getPage();
 
-	public Long getSize();
+    public Long getSize();
 
-	public Long getRecordsTotal();
+    public Long getRecordsTotal();
 
-	public List<T> getData();
+    public List<T> getData();
 
-	public String getError();
+    public String getError();
 
-	public default String getMessage() {
-		return getError();
-	}
+    public Object getMaxId();
 
-	public boolean isSuccess();
+    public default String getMessage() {
+        return getError();
+    }
 
-	/**
-	 * 使用制定的converter将分页实体数据转换为表格数据
-	 * 
-	 * @param draw
-	 * @param data
-	 * @param converter
-	 * @return
-	 */
-	public static <DTO, M> TableResult<DTO> success(Long draw, Page<M> data, Function<M, DTO> converter) {
-		TableResultDto<DTO> result = new TableResultDto<DTO>();
-		Pageable pageable = data.getPageable();
-		result.setDraw(draw);
-		result.setRecordsTotal(data.getTotalElements());
-		result.setSuccess(Boolean.TRUE);
-		result.setPage((long) pageable.getPageNumber());
-		result.setSize((long) pageable.getPageSize());
-		List<M> contents = data.getContent();
-		if (CollectionUtils.isNotEmpty(contents)) {
-			result.setData(contents.stream().map(converter).collect(Collectors.toList()));
-		} else {
-			result.setData(Collections.emptyList());
-		}
-		return result;
-	}
+    public boolean isSuccess();
 
-	public static <DTO> TableResult<DTO> success(Long draw, Page<DTO> data) {
-		return success(draw, data, d -> d);
-	}
+    /**
+     * 使用制定的converter将分页实体数据转换为表格数据
+     * 
+     * @param draw
+     * @param data
+     * @param converter
+     * @return
+     */
+    public static <DTO, M> TableResult<DTO> success(Long draw, Page<M> data, Function<M, DTO> converter) {
+        TableResultDto<DTO> result = new TableResultDto<DTO>();
+        Pageable pageable = data.getPageable();
+        result.setDraw(draw);
+        result.setRecordsTotal(data.getTotalElements());
+        result.setSuccess(Boolean.TRUE);
+        result.setPage((long) pageable.getPageNumber());
+        result.setSize((long) pageable.getPageSize());
+        List<M> contents = data.getContent();
+        if (CollectionUtils.isNotEmpty(contents)) {
+            result.setData(contents.stream().map(converter).collect(Collectors.toList()));
+        } else {
+            result.setData(Collections.emptyList());
+        }
+        return result;
+    }
 
-	/**
-	 * 返回一个表格错误信息<br />
-	 * 
-	 * @param err
-	 * @return
-	 */
-	public static <DTO, M> TableResult<DTO> failure(Long draw, Pageable pageable, String err) {
-		TableResultDto<DTO> result = new TableResultDto<>();
-		result.setDraw(draw);
-		result.setError(err);
-		result.setPage((long) pageable.getPageNumber());
-		result.setSize((long) pageable.getPageSize());
-		result.setSuccess(false);
-		return result;
-	}
+    public static <DTO> TableResult<DTO> success(Long draw, Page<DTO> data) {
+        return success(draw, data, d -> d);
+    }
 
-	public static <DTO, M> TableResult<DTO> success(TableRequest request, RangePage<M> data,
-			Function<M, DTO> converter) {
-		TableResultDto<DTO> result = new TableResultDto<DTO>();
-		result.setDraw(request.getDraw());
-		// 如果请求中有maxId,将maxId原样返回
-		if (!Objects.isNull(request.getMaxId())) {
-			result.setMaxId(request.getMaxId());
-		} else {
-			// 否则将查询出来的maxId返回
-			result.setMaxId(data.getMax());
-		}
-		Pageable pageable = data.getPageable();
-		result.setRecordsTotal(data.getTotalElements());
-		result.setSuccess(Boolean.TRUE);
-		result.setPage((long) pageable.getPageNumber());
-		result.setSize((long) pageable.getPageSize());
-		List<M> contents = data.getContent();
-		if (CollectionUtils.isNotEmpty(contents)) {
-			result.setData(contents.stream().map(converter).collect(Collectors.toList()));
-		} else {
-			result.setData(Collections.emptyList());
-		}
-		return result;
-	}
+    /**
+     * 返回一个表格错误信息<br />
+     * 
+     * @param err
+     * @return
+     */
+    public static <DTO, M> TableResult<DTO> failure(Long draw, Pageable pageable, String err) {
+        TableResultDto<DTO> result = new TableResultDto<>();
+        result.setDraw(draw);
+        result.setError(err);
+        result.setPage((long) pageable.getPageNumber());
+        result.setSize((long) pageable.getPageSize());
+        result.setSuccess(false);
+        return result;
+    }
+
+    public static <DTO, M> TableResult<DTO> success(TableRequest request, RangePage<M> data,
+            Function<M, DTO> converter) {
+        TableResultDto<DTO> result = new TableResultDto<DTO>();
+        result.setDraw(request.getDraw());
+        // 如果请求中有maxId,将maxId原样返回
+        if (!Objects.isNull(request.getMaxId())) {
+            result.setMaxId(request.getMaxId());
+        } else {
+            // 否则将查询出来的maxId返回
+            result.setMaxId(data.getMax());
+        }
+        Pageable pageable = data.getPageable();
+        result.setRecordsTotal(data.getTotalElements());
+        result.setSuccess(Boolean.TRUE);
+        result.setPage((long) pageable.getPageNumber());
+        result.setSize((long) pageable.getPageSize());
+        List<M> contents = data.getContent();
+        if (CollectionUtils.isNotEmpty(contents)) {
+            result.setData(contents.stream().map(converter).collect(Collectors.toList()));
+        } else {
+            result.setData(Collections.emptyList());
+        }
+        return result;
+    }
 }
--- dm-common/src/main/java/com/dm/common/dto/TableResultDto.java
+++ dm-common/src/main/java/com/dm/common/dto/TableResultDto.java
@@ -14,84 +14,70 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Data;
 
-/**
- * 用于表格请求的响应<br>
- * 一个正确的完整的表格响应需要包括：<br/>
- * success:查询是否成功的标记<br/>
- * draw:请求戳标记.这个标记由客户端提供，服务器原样返回该标记，用以客户端验证某次的请求是否和某次响应相匹配<br>
- * recordsTotal:查询后未分页的数据总量<br/>
- * data:在当前分页模式下，返回的数据<br/>
- * 如果表格响应错误，则应该在message中包含错误的详情
- * 
- * @author LiDong
- * 
- * 
- *
- * @param <T> 列表的数据类型
- */
+
 @JsonInclude(Include.NON_NULL)
 @Data
 class TableResultDto<T> implements TableResult<T> {
-	private static final long serialVersionUID = -7642770441688089769L;
+    private static final long serialVersionUID = -7642770441688089769L;
 
-	private Long maxId;
-	private Long draw;
-	private Long page;
-	private Long size;
-	private Long recordsTotal;
-	private List<T> data;
-	private String error;
-	private boolean success;
+    private Long maxId;
+    private Long draw;
+    private Long page;
+    private Long size;
+    private Long recordsTotal;
+    private List<T> data;
+    private String error;
+    private boolean success;
 
-	TableResultDto() {
+    TableResultDto() {
 
-	}
+    }
 
-	/**
-	 * 使用制定的converter将分页实体数据转换为表格数据<br >
-	 * 
-	 * use {@link TableResult}
-	 * 
-	 * @param draw
-	 * @param data
-	 * @param converter
-	 * @return
-	 */
+    /**
+     * 使用制定的converter将分页实体数据转换为表格数据<br >
+     * 
+     * use {@link TableResult}
+     * 
+     * @param draw
+     * @param data
+     * @param converter
+     * @return
+     */
 
-	@Deprecated
-	public static <DTO, M> TableResult<DTO> success(Long draw, Page<M> data, Function<M, DTO> converter) {
-		TableResultDto<DTO> result = new TableResultDto<DTO>();
-		Pageable pageable = data.getPageable();
-		result.setDraw(draw);
-		result.setRecordsTotal(data.getTotalElements());
-		result.setSuccess(Boolean.TRUE);
-		result.setPage((long) pageable.getPageNumber());
-		result.setSize((long) pageable.getPageSize());
-		List<M> contents = data.getContent();
-		if (CollectionUtils.isNotEmpty(contents)) {
-			result.setData(contents.stream().map(converter).collect(Collectors.toList()));
-		} else {
-			result.setData(Collections.emptyList());
-		}
-		return result;
-	}
+    @Deprecated
+    public static <DTO, M> TableResult<DTO> success(Long draw, Page<M> data, Function<M, DTO> converter) {
+        TableResultDto<DTO> result = new TableResultDto<DTO>();
+        Pageable pageable = data.getPageable();
+        result.setDraw(draw);
+        result.setRecordsTotal(data.getTotalElements());
+        result.setSuccess(Boolean.TRUE);
+        result.setPage((long) pageable.getPageNumber());
+        result.setSize((long) pageable.getPageSize());
+        List<M> contents = data.getContent();
+        if (CollectionUtils.isNotEmpty(contents)) {
+            result.setData(contents.stream().map(converter).collect(Collectors.toList()));
+        } else {
+            result.setData(Collections.emptyList());
+        }
+        return result;
+    }
 
-	/**
-	 * 返回一个表格错误信息<br />
-	 * use {@link TableResult}
-	 * 
-	 * @param err
-	 * @return
-	 */
-	@Deprecated
-	public static <DTO, M> TableResult<DTO> failure(Long draw, Pageable pageable, String err) {
-		TableResultDto<DTO> result = new TableResultDto<>();
-		result.setDraw(draw);
-		result.setError(err);
-		result.setPage((long) pageable.getPageNumber());
-		result.setSize((long) pageable.getPageSize());
-		result.setSuccess(false);
-		return result;
-	}
+    /**
+     * 返回一个表格错误信息<br />
+     * use {@link TableResult}
+     * 
+     * @param err
+     * @return
+     */
+    @Deprecated
+    public static <DTO, M> TableResult<DTO> failure(Long draw, Pageable pageable, String err) {
+        TableResultDto<DTO> result = new TableResultDto<>();
+        result.setDraw(draw);
+        result.setError(err);
+        result.setPage((long) pageable.getPageNumber());
+        result.setSize((long) pageable.getPageSize());
+        result.setSuccess(false);
+        return result;
+    }
 
 }
