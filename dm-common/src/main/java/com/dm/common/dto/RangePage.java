5632b84e1614870332ceec8a69c2584cc81bbc63
LiDong <ldwqh0@outlook.com>
2019-10-23 16:26:21 +0800

    去掉TableRequest， 修改RangePage,增加map方法

--- dm-common/src/main/java/com/dm/common/dto/RangePage.java
+++ dm-common/src/main/java/com/dm/common/dto/RangePage.java
@@ -1,6 +1,7 @@
 package com.dm.common.dto;
 
 import java.util.List;
+import java.util.function.Function;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -14,37 +15,43 @@ import org.springframework.data.domain.Pageable;
  * @param <T>
  */
 public interface RangePage<T> extends Page<T> {
-	/**
-	 * 范围的上线
-	 * 
-	 * @return
-	 */
-	public long getMax();
+    /**
+     * 范围的上线
+     * 
+     * @return
+     */
+    public long getMax();
 
-	public static <T> RangePage<T> of(long max, Page<T> page) {
-		return new RangePageImpl<T>(max, page.getContent(), page.getPageable(), page.getTotalElements());
-	}
+    public <U> RangePage<U> map(Function<? super T, ? extends U> converter);
+
+    public static <T> RangePage<T> of(long max, Page<T> page) {
+        return new RangePageImpl<T>(max, page.getContent(), page.getPageable(), page.getTotalElements());
+    }
 }
 
 class RangePageImpl<T> extends PageImpl<T> implements RangePage<T> {
 
-	private static final long serialVersionUID = -2418518649668788225L;
+    private static final long serialVersionUID = -2418518649668788225L;
 
-	private long max;
+    private long max;
 
-	public RangePageImpl(long max, List<T> content) {
-		super(content);
-		this.max = max;
-	}
+    public RangePageImpl(long max, List<T> content) {
+        super(content);
+        this.max = max;
+    }
 
-	RangePageImpl(long max, List<T> content, Pageable pageable, long total) {
-		super(content, pageable, total);
-		this.max = max;
-	}
+    RangePageImpl(long max, List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+        this.max = max;
+    }
 
-	@Override
-	public long getMax() {
-		return this.max;
-	}
+    @Override
+    public long getMax() {
+        return this.max;
+    }
 
+    @Override
+    public <U> RangePage<U> map(Function<? super T, ? extends U> converter) {
+        return new RangePageImpl<U>(max, getConvertedContent(converter), getPageable(), getTotalElements());
+    }
 }
