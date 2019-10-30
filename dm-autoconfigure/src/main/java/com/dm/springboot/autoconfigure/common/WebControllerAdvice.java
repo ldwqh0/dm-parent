4b20d73e4ccd8766f3e3e175827c055099f4fec1
LiDong <ldwqh0@outlook.com>
2019-10-30 17:30:23 +0800

    增加时间参数反序列化的方法

--- dm-autoconfigure/src/main/java/com/dm/springboot/autoconfigure/common/WebControllerAdvice.java
+++ dm-autoconfigure/src/main/java/com/dm/springboot/autoconfigure/common/WebControllerAdvice.java
@@ -1,6 +1,9 @@
 package com.dm.springboot.autoconfigure.common;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -11,8 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -29,12 +34,10 @@ public class WebControllerAdvice {
         ResponseStatus status = AnnotatedElementUtils.findMergedAnnotation(e.getClass(), ResponseStatus.class);
         if (!Objects.isNull(status)) {
             int code = status.code().value();
-//            String reason = status.reason();
             response.setStatus(code);
             Map<String, Object> result = new HashMap<String, Object>();
             result.put("path", request.getRequestURI());
             result.put("error", HttpStatus.valueOf(code).getReasonPhrase());
-//			result.put("reason", reason);
             result.put("message", e.getMessage());
             result.put("status", code);
             result.put("timestamp", ZonedDateTime.now());
@@ -43,4 +46,42 @@ public class WebControllerAdvice {
             return null;
         }
     }
+
+    // 增加时间参数反序列化的方法
+    @InitBinder
+    public void initDateFormatter(WebDataBinder binder) {
+        binder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                LocalDate l = null;
+                try {
+                    l = LocalDate.parse(text, DateTimeFormatter.ISO_DATE);
+                } catch (Exception e) {
+                }
+
+                if (Objects.isNull(l)) {
+                    l = LocalDate.parse(text, DateTimeFormatter.ISO_DATE_TIME);
+                } else {
+
+                }
+                setValue(l);
+            }
+        });
+
+        binder.registerCustomEditor(ZonedDateTime.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                ZonedDateTime z = null;
+                try {
+                    z = ZonedDateTime.parse(text, DateTimeFormatter.ISO_DATE_TIME);
+                } catch (Exception e) {
+                }
+                if (Objects.isNull(z)) {
+                    z = ZonedDateTime.parse(text, DateTimeFormatter.ISO_DATE);
+                }
+                setValue(z);
+            }
+        });
+    }
+
 }
