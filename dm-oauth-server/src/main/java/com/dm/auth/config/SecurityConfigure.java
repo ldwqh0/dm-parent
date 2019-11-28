bfa48b1f031fe6bad0629d1e707d908aa6f05fe2
LiDong <ldwqh0@outlook.com>
2019-11-28 20:39:24 +0800

    修改登录页面

--- dm-oauth-server/src/main/java/com/dm/auth/config/SecurityConfigure.java
+++ dm-oauth-server/src/main/java/com/dm/auth/config/SecurityConfigure.java
@@ -21,55 +21,57 @@ import com.dm.uap.service.LoginLogService;
 @EnableWebSecurity
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userService;
+    @Autowired
+    private UserDetailsService userService;
 
-	@Value(value = "${spring.security.default-success-url:/oauth/index.html}")
-	private String defaultSuccessUrl;
+    @Value(value = "${spring.security.default-success-url:/oauth/index.html}")
+    private String defaultSuccessUrl;
 
-	@Autowired
-	private LoginLogService loginLogService;
+    @Autowired
+    private LoginLogService loginLogService;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				// 如果不使用默认的failureHandler，需要手动指定登录失败页面，也就是login.html?error的权限为跳过
-				// 默认的跳过页面由 AbstractAuthenticationFilterConfigurer.updateAccessDefaults 设置跳过页面
-				// 设置逻辑可以参考相应源码
-				.antMatchers("/oauth/login.html**", "/styles/**", "/oauth/styles/**", "/favicon.ico").permitAll()
-				.anyRequest().authenticated()
-				.and().formLogin()
-				.loginPage("/oauth/login.html").loginProcessingUrl("/oauth/login").permitAll()
-				.failureHandler(authenticationFailureHandler())
-				.successHandler(authenticationSuccessHandler()) // .defaultSuccessUrl(defaultSuccessUrl)
-				.and().httpBasic().disable();
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 如果不使用默认的failureHandler，需要手动指定登录失败页面，也就是login.html?error的权限为跳过
+                // 默认的跳过页面由 AbstractAuthenticationFilterConfigurer.updateAccessDefaults 设置跳过页面
+                // 设置逻辑可以参考相应源码
+                .antMatchers("/oauth/login.html**", "/styles/**", "/oauth/styles/**", "/oauth/images/**",
+                        "/favicon.ico")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .loginPage("/oauth/login.html").loginProcessingUrl("/oauth/login").permitAll()
+                .failureHandler(authenticationFailureHandler())
+                .successHandler(authenticationSuccessHandler()) // .defaultSuccessUrl(defaultSuccessUrl)
+                .and().httpBasic().disable();
+    }
 
-	private AuthenticationFailureHandler authenticationFailureHandler() {
-		SimpleUrlAuthenticationLoginLogFailureHandler failureHandler = new SimpleUrlAuthenticationLoginLogFailureHandler();
-		failureHandler.setDefaultFailureUrl("/oauth/login.html?error");
-		failureHandler.setLoginLogService(loginLogService);
-		return failureHandler;
-	}
+    private AuthenticationFailureHandler authenticationFailureHandler() {
+        SimpleUrlAuthenticationLoginLogFailureHandler failureHandler = new SimpleUrlAuthenticationLoginLogFailureHandler();
+        failureHandler.setDefaultFailureUrl("/oauth/login.html?error");
+        failureHandler.setLoginLogService(loginLogService);
+        return failureHandler;
+    }
 
-	private AuthenticationSuccessHandler authenticationSuccessHandler() {
-		SavedRequestAwareAuthenticationAndLoggingSuccessHandler handler = new SavedRequestAwareAuthenticationAndLoggingSuccessHandler();
-		handler.setDefaultTargetUrl(defaultSuccessUrl);
-		handler.setAlwaysUseDefaultTargetUrl(false);
-		handler.setLoginLogService(loginLogService);
-		return handler;
-	}
+    private AuthenticationSuccessHandler authenticationSuccessHandler() {
+        SavedRequestAwareAuthenticationAndLoggingSuccessHandler handler = new SavedRequestAwareAuthenticationAndLoggingSuccessHandler();
+        handler.setDefaultTargetUrl(defaultSuccessUrl);
+        handler.setAlwaysUseDefaultTargetUrl(false);
+        handler.setLoginLogService(loginLogService);
+        return handler;
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
-		auth.userDetailsService(userService);
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.userDetailsService(userService);
+    }
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 }
