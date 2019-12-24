c1d8f1d144a622fe13494a4fd3efaa83f86836b7
LiDong <ldwqh0@outlook.com>
2019-12-24 17:06:37 +0800

    格式化和清理代码

--- dm-authority-common/src/main/java/com/dm/auth/controller/MenuAuthorityController.java
+++ dm-authority-common/src/main/java/com/dm/auth/controller/MenuAuthorityController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -68,6 +69,7 @@ public class MenuAuthorityController {
      * @return
      */
     @GetMapping("{rolename}")
+    @Transactional(readOnly = true)
     public MenuAuthorityDto get(@PathVariable("rolename") String rolename) {
         Optional<Authority> authority = authorityService.get(rolename);
         return authorityConverter.toMenuAuthorityDto(authority);
--- dm-common/src/main/java/com/dm/common/dto/RangePage.java
+++ dm-common/src/main/java/com/dm/common/dto/RangePage.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
