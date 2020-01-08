59ba12704a0af5740f2c4680e4d56a99b68c0526
LiDong <ldwqh0@outlook.com>
2020-01-08 13:50:31 +0800

    删除一个测试

--- dm-gateway-server/src/main/java/com/dm/gateway/GatewayApplication.java
+++ dm-gateway-server/src/main/java/com/dm/gateway/GatewayApplication.java
@@ -3,19 +3,10 @@ package com.dm.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-
-import com.dm.gateway.controller.GsController;
-
 @SpringBootApplication
 @EnableCaching
 public class GatewayApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Bean
-    public GsController gsController() {
-        return new GsController();
     }
 }
