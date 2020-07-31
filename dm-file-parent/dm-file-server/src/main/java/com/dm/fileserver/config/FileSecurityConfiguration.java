c14955dbaab4f9e936345dbd47b7db7abf356d5d
LiDong <ldwqh0@outlook.com>
2020-07-31 10:56:26 +0800

    恢复错误的权限配置到正确的状态

--- dm-file-parent/dm-file-server/src/main/java/com/dm/fileserver/config/FileSecurityConfiguration.java
+++ dm-file-parent/dm-file-server/src/main/java/com/dm/fileserver/config/FileSecurityConfiguration.java
@@ -24,7 +24,6 @@ public class FileSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.oauth2ResourceServer()
                 .opaqueToken().introspector(opaqueTokenIntrospector());
-        http.requestMatchers().antMatchers("/a/b");
     }
 
     @Bean
