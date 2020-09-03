377b55e8683a514dfe7c5a70b6db59b14449f626
LiDong <ldwqh0@outlook.com>
2020-09-03 11:45:34 +0800

    修改数据连接属性 DataSourceProperties实现

--- dm-jdbc/dm-multi-datasource-jpa-support/src/main/java/com/dm/datasource/converter/DmDataSourceConverter.java
+++ dm-jdbc/dm-multi-datasource-jpa-support/src/main/java/com/dm/datasource/converter/DmDataSourceConverter.java
@@ -6,9 +6,7 @@ import com.dm.datasource.dto.DmDataSourceDto;
 import com.dm.datasource.entity.DmDataSource;
 import com.dm.datasource.provider.DataSourceProperties;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class DmDataSourceConverter implements Converter<DmDataSource, DmDataSourceDto> {
 
@@ -38,12 +36,12 @@ public class DmDataSourceConverter implements Converter<DmDataSource, DmDataSour
     }
 
     public DataSourceProperties toDataSourceProperties(DmDataSourceDto from) {
-        DataSourcePropertiesDto properties = new DataSourcePropertiesDto();
+        DataSourceProperties properties = new DataSourceProperties();
         properties.setDatabase(from.getDatabase());
         properties.setDbType(from.getDbType());
         properties.setPassword(from.getPassword());
         properties.setPort(from.getPort());
-        properties.setServer(from.getHost());
+        properties.setHost(from.getHost());
         properties.setUsername(from.getUsername());
         Map<String, String> additionalProperties = from.getProperties();
         if (Maps.isNotEmpty(additionalProperties)) {
@@ -53,12 +51,12 @@ public class DmDataSourceConverter implements Converter<DmDataSource, DmDataSour
     }
 
     public DataSourceProperties toDataSourceProperties(DmDataSource model) {
-        DataSourcePropertiesDto properties = new DataSourcePropertiesDto();
+        DataSourceProperties properties = new DataSourceProperties();
         properties.setDatabase(model.getDatabase());
         properties.setDbType(model.getDbType());
         properties.setPassword(model.getPassword());
         properties.setPort(model.getPort());
-        properties.setServer(model.getHost());
+        properties.setHost(model.getHost());
         properties.setUsername(model.getUsername());
         Map<String, String> additionalProperties = model.getProperties();
         if (Maps.isNotEmpty(additionalProperties)) {
@@ -79,99 +77,5 @@ public class DmDataSourceConverter implements Converter<DmDataSource, DmDataSour
         dto.setUsername(model.getUsername());
         dto.setVersion(model.getVersion());
         return dto;
-    }
-}
-
-class DataSourcePropertiesDto implements DataSourceProperties {
-
-    private DbTypes dbType;
-    private String server;
-    private Integer port;
-    private String username;
-    private String password;
-    private String database;
-    private Map<String, String> additionalProperties = new HashMap<>();
-
-
-    @Override
-    public DbTypes getDbType() {
-        return dbType;
-    }
-
-    @Override
-    public String getHost() {
-        return server;
-    }
-
-    @Override
-    public Integer getPort() {
-        return port;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getDatabase() {
-        return database;
-    }
-
-    @Override
-    public Map<String, String> getAdditionalProperties() {
-        return additionalProperties;
-    }
-
-    public void setDbType(DbTypes dbType) {
-        this.dbType = dbType;
-    }
-
-    public void setServer(String server) {
-        this.server = server;
-    }
-
-    public void setPort(Integer port) {
-        this.port = port;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setDatabase(String database) {
-        this.database = database;
-    }
-
-    public void setProperty(String property, String value) {
-        this.additionalProperties.put(property, value);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DataSourcePropertiesDto that = (DataSourcePropertiesDto) o;
-        return Objects.equals(dbType, that.dbType) &&
-            Objects.equals(server, that.server) &&
-            Objects.equals(port, that.port) &&
-            Objects.equals(username, that.username) &&
-            Objects.equals(password, that.password) &&
-            Objects.equals(database, that.database) &&
-            Objects.equals(additionalProperties, that.additionalProperties);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(dbType, server, port, username, password, database, additionalProperties);
     }
 }
--- dm-jdbc/dm-multi-datasource/src/main/java/com/dm/datasource/provider/DataSourceProperties.java
+++ dm-jdbc/dm-multi-datasource/src/main/java/com/dm/datasource/provider/DataSourceProperties.java
@@ -1,31 +1,82 @@
 package com.dm.datasource.provider;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
-public interface DataSourceProperties {
+public final class DataSourceProperties implements Serializable {
+
+    private static final long serialVersionUID = 2331158044111375509L;
+
+    private DbTypes dbType;
+    private String host;
+    private Integer port;
+    private String username;
+
+    private String password;
+    private String database;
+    private Map<String, String> additionalProperties = new HashMap<String, String>();
+
+    public Map<String, String> getAdditionalProperties() {
+        return Collections.unmodifiableMap(additionalProperties);
+    }
 
     public enum DbTypes {
         MySQL8,
         SQLSERVER
     }
 
-    /**
-     * 数据连接的类型
-     *
-     * @return
-     */
-    public DbTypes getDbType();
+    public DbTypes getDbType() {
+        return dbType;
+    }
 
-    public String getHost();
+    public void setDbType(DbTypes dbType) {
+        this.dbType = dbType;
+    }
 
-    public Integer getPort();
+    public String getHost() {
+        return host;
+    }
 
-    public String getUsername();
+    public void setHost(String host) {
+        this.host = host;
+    }
 
-    public String getPassword();
+    public Integer getPort() {
+        return port;
+    }
 
-    public String getDatabase();
+    public void setPort(Integer port) {
+        this.port = port;
+    }
 
-    public Map<String, String> getAdditionalProperties();
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public void setProperty(String key, String value) {
+        this.additionalProperties.put(key, value);
+    }
 
 }
