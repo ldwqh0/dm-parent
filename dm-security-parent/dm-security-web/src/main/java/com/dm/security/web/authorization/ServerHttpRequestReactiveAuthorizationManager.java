ff8135745efa193df219596075ad08f76b97817a
LiDong <ldwqh0@outlook.com>
2020-10-22 14:19:08 +0800

    修改异常处理的模型

--- dm-security-parent/dm-security-web/src/main/java/com/dm/security/web/authorization/ServerHttpRequestReactiveAuthorizationManager.java
+++ dm-security-parent/dm-security-web/src/main/java/com/dm/security/web/authorization/ServerHttpRequestReactiveAuthorizationManager.java
@@ -1,10 +1,11 @@
 package com.dm.security.web.authorization;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
+import com.dm.collections.CollectionUtils;
+import com.dm.security.authentication.ResourceAuthorityAttribute;
+import com.dm.security.authentication.ResourceAuthorityService;
+import com.dm.security.authentication.UriResource;
+import com.dm.security.authentication.UriResource.MatchType;
+import com.dm.security.web.server.util.matcher.RegexServerWebExchangeMatcher;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -16,19 +17,16 @@ import org.springframework.security.web.server.authorization.AuthorizationContex
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher.MatchResult;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.web.server.ServerWebExchange;
-
-import com.dm.collections.CollectionUtils;
-import com.dm.security.authentication.ResourceAuthorityAttribute;
-import com.dm.security.authentication.ResourceAuthorityService;
-import com.dm.security.authentication.UriResource;
-import com.dm.security.authentication.UriResource.MatchType;
-import com.dm.security.web.server.util.matcher.RegexServerWebExchangeMatcher;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class ServerHttpRequestReactiveAuthorizationManager
-        implements ReactiveAuthorizationManager<AuthorizationContext> {
+    implements ReactiveAuthorizationManager<AuthorizationContext> {
 
     private ResourceAuthorityService resourceAuthorityService = null;
 
@@ -44,57 +42,58 @@ public class ServerHttpRequestReactiveAuthorizationManager
             final Collection<ResourceAuthorityAttribute> attributes = resourceAuthorityService.listAll();
             // 获取所有匹配到的数据
             Mono<List<ResourceAuthorityAttribute>> matches = Flux.fromIterable(attributes)
-                    .filterWhen(attribute -> matches(exchange, attribute).map(MatchResult::isMatch))
-                    .collectList();
+                .filterWhen(attribute -> matches(exchange, attribute).map(MatchResult::isMatch))
+                .collectList();
             return Mono.zip(matches, authentication)
-                    .flatMap(result -> check(result.getT1(), result.getT2(), context));
+                .flatMap(result -> check(result.getT1(), result.getT2(), context));
         });
     }
 
     /**
      * 判断是否有访问权限
-     * 
-     * @param list
-     * @param exchange
-     * @param currentAuthorities
-     * @return
+     *
+     * @param list           对象列表
+     * @param authentication 授权对象
+     * @param context        授权上下文
+     * @return 授权结果
      */
     private Mono<AuthorizationDecision> check(List<ResourceAuthorityAttribute> list, Authentication authentication,
-            AuthorizationContext context) {
+                                              AuthorizationContext context) {
         Flux<Boolean> checkResult = Flux.empty();
+//        Flux.fromIterable(authentication.getAuthorities());
         final Set<String> currentAuthorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .filter(StringUtils::isNotEmpty)
-                .collect(Collectors.toSet());
+            .map(GrantedAuthority::getAuthority)
+            .filter(StringUtils::isNotEmpty)
+            .collect(Collectors.toSet());
         for (ResourceAuthorityAttribute attribute : list) {
             // 如果拒绝列表中包含某一个当前用户的所属角色，立即返回
             if (CollectionUtils.isNotEmpty(currentAuthorities)
-                    && CollectionUtils.isNotEmpty(attribute.getDenyAuthorities())
-                    && CollectionUtils.containsAny(currentAuthorities,
-                            attribute.getDenyAuthorities())) {
+                && CollectionUtils.isNotEmpty(attribute.getDenyAuthorities())
+                && CollectionUtils.containsAny(currentAuthorities, attribute.getDenyAuthorities())) {
                 return Mono.just(new AuthorizationDecision(false));
             }
-
             // 匿名用户的可访问权限包含在显示的权限配置中
+
             if (CollectionUtils.isNotEmpty(currentAuthorities)
-                    && CollectionUtils.isNotEmpty(attribute.getAccessAuthority())
-                    && CollectionUtils.containsAny(currentAuthorities,
-                            attribute.getAccessAuthority())) {
+                && CollectionUtils.isNotEmpty(attribute.getAccessAuthority())
+                && CollectionUtils.containsAny(currentAuthorities, attribute.getAccessAuthority())) {
                 checkResult = checkResult.concatWith(additionalValidate(attribute, authentication, context));
             }
         }
-        return checkResult.all(Boolean.TRUE::equals).map(AuthorizationDecision::new);
+        return checkResult.defaultIfEmpty(Boolean.FALSE)
+            .all(Boolean::booleanValue)
+            .map(AuthorizationDecision::new);
     }
 
     /**
      * 进行额外的校验
-     * 
+     *
      * @param attribute
      * @param authentication
      * @return
      */
     protected Mono<Boolean> additionalValidate(ResourceAuthorityAttribute attribute, Authentication authentication,
-            AuthorizationContext context) {
+                                               AuthorizationContext context) {
         return Mono.just(Boolean.TRUE);
     }
 
@@ -104,13 +103,13 @@ public class ServerHttpRequestReactiveAuthorizationManager
         MatchType matchType = resource.getMatchType();
         if (MatchType.ANT_PATH.equals(matchType)) {
             return ServerWebExchangeMatchers
-                    .pathMatchers(HttpMethod.resolve(resource.getMethod()), resource.getPath())
-                    .matches(exchange);
+                .pathMatchers(HttpMethod.resolve(resource.getMethod()), resource.getPath())
+                .matches(exchange);
         }
         if (MatchType.REGEXP.equals(matchType)) {
             return ServerWebExchangeMatchers
-                    .matchers(new RegexServerWebExchangeMatcher(resource.getPath(), resource.getMethod()))
-                    .matches(exchange);
+                .matchers(new RegexServerWebExchangeMatcher(resource.getPath(), resource.getMethod()))
+                .matches(exchange);
         }
         return Mono.empty();
     }
