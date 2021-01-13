7c1d2b906db3968670d78472d74213543af035a3
ldwqh0 <ldwqh0@outlook.com>
2021-01-13 18:06:15 +0800

    添加session保活接口

--- dm-security-parent/dm-security-web/src/main/java/com/dm/security/web/controller/CurrentUserController.java
+++ dm-security-parent/dm-security-web/src/main/java/com/dm/security/web/controller/CurrentUserController.java
@@ -1,10 +1,11 @@
 package com.dm.security.web.controller;
 
+import com.dm.security.annotation.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.dm.security.annotation.CurrentUser;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class CurrentUserController {
@@ -12,5 +13,11 @@ public class CurrentUserController {
     @ResponseBody
     public Object currentUser(@CurrentUser Object p) {
         return p;
+    }
+
+    @GetMapping("session")
+    @ResponseBody
+    public Object session(WebRequest request) {
+        return request.getSessionId();
     }
 }
