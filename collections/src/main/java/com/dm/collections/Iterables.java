51180b392b00c599ead5ff69be9d4a9772efed90
LiDong <ldwqh0@outlook.com>
2021-04-21 11:56:03 +0800

    增加临时配置,解决application/x-www-form-urlencoded的数据提交问题

--- collections/src/main/java/com/dm/collections/Iterables.java
+++ collections/src/main/java/com/dm/collections/Iterables.java
@@ -1,5 +1,11 @@
 package com.dm.collections;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public final class Iterables {
     private Iterables() {
     }
@@ -10,5 +16,17 @@ public final class Iterables {
 
     public static boolean isNotEmpty(Iterable<?> iterable) {
         return !isEmpty(iterable);
+    }
+
+    public static <T> Stream<T> toStream(Iterable<T> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    public static <T> Stream<T> toStream(Iterable<T> iterable, boolean parallel) {
+        return StreamSupport.stream(iterable.spliterator(), parallel);
+    }
+
+    public static <I, R> List<R> mapToList(Iterable<I> iterable, Function<I, R> converter) {
+        return StreamSupport.stream(iterable.spliterator(), false).map(converter).collect(Collectors.toList());
     }
 }
