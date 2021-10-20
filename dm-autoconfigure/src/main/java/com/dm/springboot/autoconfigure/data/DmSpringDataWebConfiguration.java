d692a4d5a2fb989ee19bdc1b144d1ccf7aafa469
ldwqh0 <ldwqh0@outlook.com>
2021-10-20 19:16:22 +0800

    修改私有仓库地址

--- dm-autoconfigure/src/main/java/com/dm/springboot/autoconfigure/data/DmSpringDataWebConfiguration.java
+++ dm-autoconfigure/src/main/java/com/dm/springboot/autoconfigure/data/DmSpringDataWebConfiguration.java
@@ -4,12 +4,14 @@ import com.dm.data.domain.RangePageable;
 import com.dm.data.web.RangePageableArgumentResolver;
 import com.dm.data.web.RangePageableHandlerMethodArgumentResolver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @ConditionalOnClass(RangePageable.class)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class DmSpringDataWebConfiguration implements WebMvcConfigurer {
 
     @Override
