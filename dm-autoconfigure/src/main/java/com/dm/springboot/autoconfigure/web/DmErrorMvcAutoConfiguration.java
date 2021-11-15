cbfbce691b6b6ff245c13d1d830e33c00b8064db
ldwqh0 <ldwqh0@outlook.com>
2021-11-15 16:13:03 +0800

    修改错误处理逻辑，增加异常信息

---
+++ dm-autoconfigure/src/main/java/com/dm/springboot/autoconfigure/web/DmErrorMvcAutoConfiguration.java
@@ -0,0 +1,35 @@
+package com.dm.springboot.autoconfigure.web;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.Servlet;
+
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@ConditionalOnClass({Servlet.class, DispatcherServlet.class})
+@AutoConfigureBefore(WebMvcAutoConfiguration.class)
+public class DmErrorMvcAutoConfiguration {
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+            @Override
+            protected String getMessage(WebRequest webRequest, Throwable error) {
+                String message = error.getMessage();
+                if (StringUtils.isEmpty(message)) {
+                    return super.getMessage(webRequest, error);
+                } else {
+                    return message;
+                }
+            }
+        };
+    }
+}
