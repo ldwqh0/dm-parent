7c268395be75181b785cdd73807bc90cef37b7a9
ldwqh0 <ldwqh0@outlook.com>
2021-11-21 22:18:41 +0800

    删除pom.xml文件

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-git.exe push --progress "origin" 2.2.x:2.2.x
-git.exe push --progress "origin" master:master
-git.exe push --progress "aliyun" 2.2.x:develop
-git.exe push --progress "aliyun" master:master
-mvn clean deploy -Dmaven.test.skip=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true
