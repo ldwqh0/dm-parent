d9329e27d653dc932e0439c1fdc285c960f0fa65
ldwqh0 <ldwqh0@outlook.com>
2022-02-10 14:25:47 +0800

    修改bom编译构建配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,57 @@ javaPlatform {
     allowDependencies()
 }
 dependencies {
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:${V.V.springCloud}"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:${V.V.springBoot}"))
     constraints {
-        runtime("jakarta.activation:jakarta.activation-api:1.2.2")
+        api(project(":dm-common"))
+        api(project(":collections"))
+        api(project(":dm-file-core"))
+        api(project(":dm-file-stream"))
+        api(project(":dm-file-spring-boot-starter"))
+        api(project(":dm-autoconfigure"))
+        api(project(":dm-authority-common"))
+        api(project(":dm-security-core"))
+        api(project(":dm-security-web"))
+        api(project(":dm-multi-datasource"))
+        api(project(":dm-security-mp"))
+        api(project(":dm-security-oauth2-core"))
+        api(project(":dm-security-oauth2-resource"))
+        api(project(":dm-security-oauth2-client"))
+        api(project(":dm-security-spring-boot-starter"))
+        api(project(":dm-oauth2-resource-server-spring-boot-starter"))
+        api(project(":dm-oauth2-client-spring-boot-starter"))
+        api(project(":dm-multi-datasource-jpa-support"))
+        api(project(":dm-region"))
+        api(project(":dm-uap-common"))
+        api(project(":dm-notification-api"))
+        api(project(":dm-notification-client"))
+        api(project(":dm-notification-server"))
+        api(project(":dm-todo-task-api"))
+        api(project(":dm-todo-task-client"))
+        api(project(":dm-todo-task-server"))
     }
 }
+
+configurations.all {
+    exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+}
+
 publishing {
     publications.create<MavenPublication>("maven") {
         from(components["javaPlatform"])
     }
     repositories {
+        maven {
+            val releasesRepoUrl = "http://demo.yzhxh.com:8081/nexus/repository/maven-releases/"
+            val snapshotsRepoUrl = "http://demo.yzhxh.com:8081/nexus/repository/maven-snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            isAllowInsecureProtocol = true
+            credentials {
+                username = "lidong"
+                password = "lidong"
+            }
+        }
         mavenLocal()
     }
 }
