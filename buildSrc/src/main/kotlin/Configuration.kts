693aafe1cb8558cba028b79aca6f0ecc9885f4dd
xz <ldwqh0@outlook.com>
2022-04-06 16:19:27 +0800

    升级版本

--- buildSrc/src/main/kotlin/Configuration.kts
+++ buildSrc/src/main/kotlin/Configuration.kts
@@ -1,7 +1,7 @@
 object Versions {
     const val project = "2.3.0-SNAPSHOT"
-    const val springBoot = "2.6.3"
-    const val springCloud = "2021.0.0"
+    const val springBoot = "2.6.6"
+    const val springCloud = "2021.0.1"
     const val queryDsl = "5.0.0"
     const val commons_io = "2.11.0"
     const val jsoup = "1.14.3"
