0ebfd8085affe28e603bbfc496ac76178fb4b23e
xz <ldwqh0@outlook.com>
2022-06-24 17:25:06 +0800

    添加当前用户的角色接口的文档

--- dm-security/dm-security-web/src/main/java/com/dm/security/web/controller/CurrentAuthorityController.java
+++ dm-security/dm-security-web/src/main/java/com/dm/security/web/controller/CurrentAuthorityController.java
@@ -1,20 +1,36 @@
 package com.dm.security.web.controller;
 
-import com.dm.security.annotation.CurrentUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
+/**
+ * 当前用户授权和会话
+ */
 @Controller
 public class CurrentAuthorityController {
+    /**
+     * 获取当前页用户信息
+     *
+     * @param principal 用户信息
+     * @ignoreParams principal
+     */
     @GetMapping({"/authorities/current", "/p/authorities/current"})
     @ResponseBody
-    public Object currentUser(@CurrentUser Object p) {
-        return p;
+    public Object currentUser(@AuthenticationPrincipal Object principal) {
+        return principal;
     }
 
+    /**
+     * 获取当前会话id
+     *
+     * @param request 请求
+     * @return 会话的id
+     * @ignoreParams request
+     */
     @ResponseBody
     @RequestMapping("session")
     public String session(WebRequest request) {
