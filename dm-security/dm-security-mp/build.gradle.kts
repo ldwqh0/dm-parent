7f84566a1e973655c44ac3fe24756b9c62d45ec8
xz <ldwqh0@outlook.com>
2022-07-06 18:50:11 +0800

    修改微信公众号登录的逻辑和配置

--- dm-security/dm-security-mp/build.gradle.kts
+++ dm-security/dm-security-mp/build.gradle.kts
@@ -5,10 +5,13 @@ dependencies {
     implementation("org.springframework.security:spring-security-core")
     implementation("org.springframework.security:spring-security-web")
     implementation(project(":collections"))
-
-    api("com.github.binarywang:weixin-java-mp")
-
+//    implementation("com.dm:collections")
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.apache.commons:commons-lang3")
     implementation("com.fasterxml.jackson.core:jackson-annotations")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+//    api("com.github.binarywang:weixin-java-mp")
+
     compileOnly("jakarta.servlet:jakarta.servlet-api")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api")
